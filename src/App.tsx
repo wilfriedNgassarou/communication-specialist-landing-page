693c1732e48423bc5739efb30a49368045f0109d
unknown <wilfriedngassarou23@gmail.com>
2025-01-16 17:35:55 +0100

    Updated App.tsx (contact, discussion, footer, portofolio, testimonials componenets)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { About } from "./components/about/about"
+import { Contact } from "./components/contact/contact"
+import { Discussion } from "./components/discussion/discussion"
+import { Footer } from "./components/footer/footer"
 import { Header } from "./components/header/header"
 import { Hero } from "./components/hero/hero"
+import { Portofolios } from "./components/portofolios/portofolios"
+import { Testimonials } from "./components/testimonials/testimonials"
 import { WhatIDo } from "./components/what-i-do/what-i-do"
 import { WorkProcess } from "./components/work-process/work-process"
 
@@ -14,6 +19,11 @@ function App() {
       <About />
       <WorkProcess />
       <WhatIDo />
+      <Portofolios />
+      <Discussion />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </>
   )
 }
