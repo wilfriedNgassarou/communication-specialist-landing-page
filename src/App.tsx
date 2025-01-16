e76dc051ef086a1774be22eb7f1c8b73866ae7a5
unknown <wilfriedngassarou23@gmail.com>
2025-01-16 14:59:23 +0100

    Updated App.tsx to add some components (about, what i do, work process)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { About } from "./components/about/about"
 import { Header } from "./components/header/header"
 import { Hero } from "./components/hero/hero"
+import { WhatIDo } from "./components/what-i-do/what-i-do"
+import { WorkProcess } from "./components/work-process/work-process"
 
 function App() {
   return (
     <>
-      <section className="w-full h-dvh flex flex-col">
+      <section className="w-full lg:h-dvh flex flex-col">
         <Header />
         <Hero />
       </section>
+      <About />
+      <WorkProcess />
+      <WhatIDo />
     </>
   )
 }
